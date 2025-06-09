b1e093f54424f84c8df2d569d84e6a4d45b4e6ec
FranRDev <franciscorodriguezgar@gmail.com>
2025-06-09 10:21:20 +0200

    Clase 12. InputSignal y HostBindings

--- 01-calculadora/src/app/calculadora/components/boton-calculadora/boton-calculadora.component.ts
+++ 01-calculadora/src/app/calculadora/components/boton-calculadora/boton-calculadora.component.ts
@@ -1,14 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  selector: 'boton-calculadora',
-  templateUrl: './boton-calculadora.component.html',
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400',
     // attribute: 'hola',
     // 'data-size': 'XL'
-  }
+  },
+  imports: [],
+  selector: 'boton-calculadora',
+  styleUrl: './boton-calculadora.component.css',
+  templateUrl: './boton-calculadora.component.html'
 })
-export class BotonCalculadoraComponent { }
+export class BotonCalculadoraComponent {
+
+  public comando = input(false, { transform: (valor: boolean | string) => typeof valor === 'string' ? valor === '' : valor });
+
+  @HostBinding('class.comando') get estiloComando() {
+    return this.comando();
+  }
+
+}
