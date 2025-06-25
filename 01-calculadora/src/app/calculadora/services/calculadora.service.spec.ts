a59ca1681dabf24fcd5d5ac09c7a6d309eb05dfd
FranRDev <franciscorodriguezgar@gmail.com>
2025-06-25 12:55:44 +0200

    Clase 36. Ciclo de vida - ReInicialización

--- 01-calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ 01-calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -5,16 +5,32 @@ import { CalculadoraService } from "./calculadora.service";
 describe('CalculadoraService', () => {
   let servicio: CalculadoraService;
 
-  beforeEach(() => {
+  beforeEach(() => { // Antes de cada prueba.
     TestBed.configureTestingModule({});
     servicio = TestBed.inject(CalculadoraService);
   });
+
+  beforeAll(() => {}); // Antes de todas las pruebas.
+  afterEach(() => {}); // Después de cada prueba.
+  afterAll(() => {}); // Después de todas las pruebas.
 
   it('debería ser creado', () => {
     expect(servicio).toBeTruthy();
   });
 
   it('debería crearse con valores por defecto', () => {
+    expect(servicio.textoResultado()).toBe('0');
+    expect(servicio.textoSubresultado()).toBe('0');
+    expect(servicio.ultimoOperador()).toBe('+');
+  });
+
+  it('debería establecerse resultado y subresultado a 0 presionando C', () => {
+    servicio.textoResultado.set('123');
+    servicio.textoSubresultado.set('456');
+    servicio.ultimoOperador.set('*');
+
+    servicio.construirNumero('C');
+
     expect(servicio.textoResultado()).toBe('0');
     expect(servicio.textoSubresultado()).toBe('0');
     expect(servicio.ultimoOperador()).toBe('+');
