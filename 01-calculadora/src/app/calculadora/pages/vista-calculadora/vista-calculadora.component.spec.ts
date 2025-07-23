11c7ff1d28bbd36239e1b2269faa6e38771bce04
FranRDev <franciscorodriguezgar@gmail.com>
2025-07-23 08:12:19 +0200

    Clase 39. Pruebas en CalculatorView

---
+++ 01-calculadora/src/app/calculadora/pages/vista-calculadora/vista-calculadora.component.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import VistaCalculadoraComponent from './vista-calculadora.component';
+
+describe('VistaCalculadoraComponent', () => {
+
+  let fixture: ComponentFixture<VistaCalculadoraComponent>;
+  let compiled: HTMLElement;
+  let component: VistaCalculadoraComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VistaCalculadoraComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(VistaCalculadoraComponent);
+    compiled = fixture.nativeElement as HTMLElement;
+    component = fixture.componentInstance;
+  });
+
+  it('debería crear la app', () => {
+    console.log(compiled);
+    expect(component).toBeTruthy()
+  });
+
+  it('debería contener el componente calculadora', () => expect(compiled.querySelector('calculadora')).not.toBeNull());
+
+  it('debería contener las clases CSS básicas', () => {
+    const clasesBasicas = 'w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden'.split(' ');
+    const div = compiled.querySelector('div');
+    const clasesDiv = div?.classList.value.split(' ');
+    clasesBasicas.forEach(clase => expect(clasesDiv).toContain(clase));
+  });
+
+});
